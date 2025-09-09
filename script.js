442bc91205c3af6ebff88a84b28bdd44fa0242e2
Noah Miguel Perez <noahm.p.in2017@gmail.com>
2025-09-09 12:55:06 -0400

    Added timeline animations

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ let id;
 let op = 0.0;
 
 function scroll() {
+
+    // Directory animations
     var delta = (prevY - window.scrollY) * 0.5;
     scrollval = Math.max(0, Math.min(100, scrollval+delta));
     for (let element of document.getElementById("directory").children) {
@@ -12,6 +14,24 @@ function scroll() {
     document.getElementById("directory").style.backgroundColor = "rgba(4, 25, 45, " + scrollval/100.0 + ")";
     document.getElementById("directory").style.boxShadow = "0 0 7px rgba(0, 0, 0, " + scrollval/100.0 + ")";
     prevY = window.scrollY;
+
+    // Timeline animations
+    var rect;
+    var targetYear;
+    for (let div of document.getElementById("projects").children) {
+        if (div.nodeName != "DIV") continue;
+        rect = div.getBoundingClientRect();
+        if (screen.height * 0.5 >= rect.y && rect.y >= -(+rect.height)) {
+            targetYear = div.className;
+        }
+    }
+    for (let yearDiv of document.getElementById("timeline").children) {
+       if (yearDiv.className == targetYear) {
+            yearDiv.children[0].style.fontSize = "30px";
+       } else {
+            yearDiv.children[0].style.fontSize = "20px";
+       }
+    }
 }
 
 function fadeIn(element) {
